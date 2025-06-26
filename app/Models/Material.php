24c05ae8bc1dfa6d0b54067d9817cfbf82bd7de8
Gerardo Gael Rojas Ramos <gerardo.rojasramos@ucr.ac.cr>
2025-06-25 18:40:28 -0600

    Gerardo Rojas: Clase y Migracion - Material.

---
+++ app/Models/Material.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Material extends Model
+{
+    protected $table = 'materiales';
+    protected $primaryKey = 'codigo';
+
+    protected $fillable = [
+        'unidadMedida',
+        'descripcion',
+        'ubicacion',
+        'idCategoria'
+    ];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class, 'idCategoria', 'idCategoria');
+    }
+}
